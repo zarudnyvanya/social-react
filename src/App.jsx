4ef7b54eb3d44a5254cc800d512d9b13cfed7bd4
quro4ka <quro4ka@gmail.com>
2022-07-10 15:39:12 +0300

    add components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Nav />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <a href="#">Profile</a>
+        </li>
+        <li>
+          <a href="#">Messages</a>
+        </li>
+        <li>
+          <a href="#">News</a>
+        </li>
+        <li>
+          <a href="#">Music</a>
+        </li>
+        <li>
+          <a href="#">Settings</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
